a243c3862abbcbb82ff83205adcac441a0774a77
ravi2003 <ravikant.vishwakarma@inscripts.com>
2019-02-23 23:09:35 -0800

    first commit

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Cart extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello fro cart</h3>
+      </div>
+    )
+  }
+}
---
+++ src/context.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import {storeProducts,detailProduct} from './data';
+const ProductContext = React.createContext();
+
+
+class ProductProvider extends Component {
+    state = {
+        products:[],
+        detailProduct:detailProduct
+    }
+    componentDidMount(){
+      this.setProducts();
+    }
+    setProducts =()=>{
+      let temProducts = [];
+      storeProducts.forEach(item=>{
+        const singleItem = {...item};
+        temProducts = [...temProducts,singleItem];        
+      })
+      this.setState(()=>{
+        return {products:temProducts}
+      })
+    }
+    handleDetail = () =>{
+        console.log("hellofrom dteials");
+    }
+    addToCart = () =>{
+        console.log("hello ffrom add to crat")
+    }
+  render() {
+    return (
+      <ProductContext.Provider 
+            value ={{
+                ...this.state,
+                handleDetail:this.handleDetail,
+                addToCart:this.addToCart
+            }}
+        >
+        {this.props.children}
+      </ProductContext.Provider>
+    )
+  }
+}
+
+const ProductConsumer=ProductContext.Consumer;
+
+export { ProductProvider,ProductConsumer };
